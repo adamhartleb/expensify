2d9d802a7372ddd319b436b816cf203d431b41c8
Adam <adamjhartleb@gmail.com>
2017-09-12 14:15:24 -0700

    added express erver

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('public'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
+})
+
+app.listen(3000, () => console.log('Listening on 3000'))
