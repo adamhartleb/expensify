09f87bc4a4d3411059347b97b1a6fef3c91d3c03
Adam <adamjhartleb@gmail.com>
2017-09-12 14:16:50 -0700

    setup for heroku

--- server/server.js
+++ server/server.js
@@ -8,4 +8,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
 })
 
-app.listen(3000, () => console.log('Listening on 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('Listening on 3000'))
