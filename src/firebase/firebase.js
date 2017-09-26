bde9385b1442fc789d42e9cb04c739ca1ac16a7f
Adam <adamjhartleb@gmail.com>
2017-09-26 15:07:13 -0700

    set up for heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 const config = {
-	apiKey: 'AIzaSyCEbvZUf6U4IvPOcLO9upT4GdabnMqjx9k',
-	authDomain: 'expensify-7b6ed.firebaseapp.com',
-	databaseURL: 'https://expensify-7b6ed.firebaseio.com',
-	projectId: 'expensify-7b6ed',
-	storageBucket: 'expensify-7b6ed.appspot.com',
-	messagingSenderId: '1039516606316'
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
 
 firebase.initializeApp(config)
